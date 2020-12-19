030f9f665a4d61af90da39a84f7d9aba167f78e8
Federico Matteoni <fedex15@live.it>
2020-12-19 11:26:28 +0100

    Warnings

--- app/src/main/java/com/fexed/rpgsheet/DiceDialog.java
+++ app/src/main/java/com/fexed/rpgsheet/DiceDialog.java
@@ -3,20 +3,16 @@ package com.fexed.rpgsheet;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
 import android.text.InputType;
-import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 
